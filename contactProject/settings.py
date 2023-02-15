ed9b4a2fa7704e34e12783bc7e2f48942efde8ec
Aziz <92602264+AMG2ks@users.noreply.github.com>
2023-02-15 09:27:57 +0000

    Update settings.py

--- contactProject/settings.py
+++ contactProject/settings.py
@@ -162,7 +162,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gabsiaziz37@gmail.com'
-EMAIL_HOST_PASSWORD = 'xcnqqyvxgqpzkxaf'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DJOSER = {
     'LOGIN_FIELD': 'email',
